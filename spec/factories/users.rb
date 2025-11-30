8ba2d557a89891695a4acf57f4f54a84277ac15a
Hasibul Huda <hasibul.huda@welldev.io>
2025-11-30 17:45:41 +0600

    feat: add basic tests for notification and check_result models

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,14 +4,3 @@ FactoryBot.define do
     password { "password123" }
   end
 end
-
-# spec/factories/monitored_sites.rb
-FactoryBot.define do
-  factory :monitored_site do
-    association :user
-    name { Faker::App.name }
-    url { Faker::Internet.url }
-    check_frequency_seconds { 300 }
-    last_status { :pending }
-  end
-end
