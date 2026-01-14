83bfcd0a1608629385b2a5af9f5bc971cfcf028f
Hasibul Huda <hasibul.huda@welldev.io>
2026-01-14 13:06:24 +0600

    Fix failing test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/users/show"
-      expect(response).to have_http_status(:success)
+RSpec.describe "User", type: :request do
+  let!(:user) { create(:user) }
+  describe "GET /user" do
+    context "when user is not signed in" do
+      it "redirects to the login page" do
+        get "/user"
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context "when user is signed in" do
+      before do
+        sign_in user
+      end
+
+      it "renders the user profile successfully" do
+        get "/user"
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
 end
