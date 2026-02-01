3b80d5f375b8d2649c7ccac2bee1beaa9c41a6bf
Hasibul Huda <hasibul.huda@welldev.io>
2026-02-01 12:39:37 +0600

    [task] Improve logging for site check job

--- app/jobs/schedule_site_check_job.rb
+++ app/jobs/schedule_site_check_job.rb
@@ -2,17 +2,31 @@ class ScheduleSiteCheckJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    sites_due = MonitoredSite.ready_for_check
+    Rails.logger.tagged("Scheduler") do
+      sites_due = MonitoredSite.ready_for_check
 
-    puts sites_due.inspect
+      if sites_due.any?
+        Rails.logger.info "Found #{sites_due.count} sites ready for checking."
+      else
+        Rails.logger.debug "No sites due for check."
+        return
+      end
 
-    sites_due.each do |site|
-      MonitorSiteJob.perform_later(site)
+      sites_due.each do |site|
+        Rails.logger.info "Queuing check for Site ID: #{site.id} (#{site.url})"
 
-      site.update_column(
-        :next_check_at,
-        Time.current + site.check_frequency_seconds.seconds
-      )
+        MonitorSiteJob.perform_later(site)
+
+        site.update_column(
+          :next_check_at,
+          Time.current + site.check_frequency_seconds.seconds
+        )
+      end
+
+      Rails.logger.info "Batch queuing complete."
     end
+  rescue => e
+    Rails.logger.error "[Scheduler] CRITICAL FAILURE: #{e.message}"
+    Rails.logger.error e.backtrace.first(3).join("\n")
   end
 end
