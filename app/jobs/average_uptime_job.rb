31f8e1b2985d39d99debefd43545e7867f2c404b
Hasibul Huda <hasibul.huda@welldev.io>
2026-02-01 13:17:53 +0600

    [feat] Improve logging

--- app/jobs/average_uptime_job.rb
+++ app/jobs/average_uptime_job.rb
@@ -4,19 +4,24 @@ class AverageUptimeJob < ApplicationJob
   queue_as :default
 
   def perform(monitored_site)
-    average = monitored_site.average_response_time
+    Rails.logger.tagged("Analytics", "SiteID: #{monitored_site.id}") do
+      average = monitored_site.average_response_time
+      target_id = dom_id(monitored_site, :average_response_time)
 
-    puts "Performing average check. Average is: ", average
+      Rails.logger.info "Recalculated average response time: #{average}ms"
 
-    target_id = dom_id(monitored_site, :average_response_time)
+      Rails.logger.debug "Broadcasting update to DOM ID: #{target_id}"
 
-    puts "Target for average is: ", target_id
+      Turbo::StreamsChannel.broadcast_replace_to(
+        monitored_site,
+        target: target_id,
+        partial: "monitored_sites/avg_time",
+        locals: { average: average, monitored_site: monitored_site }
+      )
 
-    Turbo::StreamsChannel.broadcast_replace_to(
-      monitored_site,
-      target: target_id,
-      partial: "monitored_sites/avg_time",
-      locals: { average: average, monitored_site: monitored_site }
-    )
+      Rails.logger.info "UI Update Broadcast Complete."
+    end
+  rescue => e
+    Rails.logger.error "FAILED to update average uptime for Site #{monitored_site.id}: #{e.message}"
   end
 end
